2caac31c1a9e165b87a653ea7ef0f62b596f2926
Xolmo <a20220378@pucp.edu.pe>
2025-05-30 20:11:02 -0500

    Initial Commit

---
+++ src/main/java/com/example/lab8_20220378/Lab820220378Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20220378;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820220378Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab820220378Application.class, args);
+    }
+
+}
